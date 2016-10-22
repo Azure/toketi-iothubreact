e48ed2ce792127245c5466e28815155bc0988b0c
Devis Lucato <devis@lucato.it>
2016-10-21 17:48:25 -0700

    Add checkpointing logic

---
+++ src/main/scala/com/microsoft/azure/iot/iothubreact/checkpointing/CheckpointActorSystem.scala
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+package com.microsoft.azure.iot.iothubreact.checkpointing
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
+
+import scala.language.{implicitConversions, postfixOps}
+
+/** The actors infrastructure for storing the stream position
+  */
+private[iothubreact] object CheckpointActorSystem {
+
+  implicit private[this] val actorSystem  = ActorSystem("IoTHubReact")
+  implicit private[this] val materializer = ActorMaterializer(ActorMaterializerSettings(actorSystem))
+  var localRegistry: Map[String, ActorRef] = Map[String, ActorRef]()
+
+  /** Create an actor to read/write offset checkpoints from the storage
+    *
+    * @param partition IoT hub partition number
+    *
+    * @return Actor reference
+    */
+  def getCheckpointService(partition: Int): ActorRef = {
+    val actorPath = "CheckpointService" + partition
+
+    localRegistry get actorPath match {
+      case Some(actorRef) => actorRef
+      case None           => {
+        val actorRef = actorSystem.actorOf(Props(new CheckpointService(partition)), actorPath)
+        localRegistry += Tuple2(actorPath, actorRef)
+        actorRef
+      }
+    }
+  }
+}
