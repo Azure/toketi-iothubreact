548ea07d0cfaac3402fd192a841915389c83b454
Devis Lucato <devis@lucato.it>
2016-11-08 15:47:15 -0800

    Remove tools

--- tools/cloud-to-device-receive/src/main/scala/Main.scala
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-
-/** Receive messages sent from Cloud to IoT Devices
-  */
-object Main extends App {
-
-}
--- tools/cloud-to-devices-send/src/main/scala/Main.scala
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-
-/** Send messages from Cloud to IoT Devices
-  */
-object Main extends App {
-
-}
