a3d54c1dd0a7fe99173f7fbdcc2d38b4f042e3d9
Devis Lucato <devis@lucato.it>
2017-01-04 18:01:51 -0800

    Comment out sinks blocked by SDK work-in-progress

--- src/main/scala/com/microsoft/azure/iot/iothubreact/sinks/DevicePropertiesSink.scala
+++ src/main/scala/com/microsoft/azure/iot/iothubreact/sinks/DevicePropertiesSink.scala
@@ -1,13 +1,22 @@
 // Copyright (c) Microsoft. All rights reserved.
 
+/*
+
+TODO: Develop once SDK is ready
+
 package com.microsoft.azure.iot.iothubreact.sinks
 
 import akka.Done
 import akka.stream.scaladsl.Sink
-import com.microsoft.azure.iot.iothubreact.{DeviceProperties, TypedSink}
+import com.microsoft.azure.iot.iothubreact.DeviceProperties
 
 import scala.concurrent.Future
 
 case class DevicePropertiesSink() {
-  def sink(): Sink[DeviceProperties, Future[Done]] = Sink.ignore
+  def sink(): Sink[DeviceProperties, Future[Done]] = Sink.foreach[DeviceProperties] {
+    m ⇒ {
+
+    }
+  }
 }
+*/
--- src/main/scala/com/microsoft/azure/iot/iothubreact/sinks/MethodOnDeviceSink.scala
+++ src/main/scala/com/microsoft/azure/iot/iothubreact/sinks/MethodOnDeviceSink.scala
@@ -1,13 +1,22 @@
 // Copyright (c) Microsoft. All rights reserved.
 
+/*
+
+TODO: Develop once SDK is ready
+
 package com.microsoft.azure.iot.iothubreact.sinks
 
 import akka.Done
 import akka.stream.scaladsl.Sink
-import com.microsoft.azure.iot.iothubreact.{MethodOnDevice, TypedSink}
+import com.microsoft.azure.iot.iothubreact.MethodOnDevice
 
 import scala.concurrent.Future
 
 case class MethodOnDeviceSink() {
-  def sink(): Sink[MethodOnDevice, Future[Done]] = Sink.ignore
+  def sink(): Sink[MethodOnDevice, Future[Done]] = Sink.foreach[MethodOnDevice] {
+    m ⇒ {
+
+    }
+  }
 }
+*/
