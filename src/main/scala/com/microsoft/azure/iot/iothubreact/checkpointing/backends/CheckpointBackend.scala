c181b729eb177f0a6ec2fd29e1acf42167acc715
Devis Lucato <devis@lucato.it>
2017-03-10 14:49:38 -0800

    Move “Backends” folder content to “backends” (merging “backends” with “Backends”)

---
+++ src/main/scala/com/microsoft/azure/iot/iothubreact/checkpointing/backends/CheckpointBackend.scala
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+package com.microsoft.azure.iot.iothubreact.checkpointing.backends
+
+import com.microsoft.azure.iot.iothubreact.checkpointing.Configuration
+
+trait CheckpointBackend {
+
+  def checkpointNamespace: String = Configuration.storageNamespace
+
+  /** Read the offset of the last record processed for the given partition
+    *
+    * @param partition IoT hub partition number
+    *
+    * @return Offset of the last record (already) processed
+    */
+  def readOffset(partition: Int): String
+
+  /** Store the offset for the given IoT hub partition
+    *
+    * @param partition IoT hub partition number
+    * @param offset    IoT hub partition offset
+    */
+  def writeOffset(partition: Int, offset: String): Unit
+}
