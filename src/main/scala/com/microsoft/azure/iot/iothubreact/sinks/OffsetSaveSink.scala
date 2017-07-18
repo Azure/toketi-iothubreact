a972ec11979210a07aeb4f5b78999d672b3f6a2b
Keith Nordstrom <keith.nordstrom@timeseriesgroup.com>
2017-07-17 20:53:02 -0600

    Some cleanup in OffsetSaveSink

--- src/main/scala/com/microsoft/azure/iot/iothubreact/sinks/OffsetSaveSink.scala
+++ src/main/scala/com/microsoft/azure/iot/iothubreact/sinks/OffsetSaveSink.scala
@@ -3,17 +3,14 @@
 package com.microsoft.azure.iot.iothubreact.sinks
 
 import java.util.concurrent.CompletionStage
-import java.util.concurrent.atomic.AtomicLong
 
 import akka.Done
 import akka.actor.ActorRef
-import akka.japi.function.Procedure
 import akka.stream.javadsl.{Sink ⇒ JavaSink}
 import akka.stream.scaladsl.{Sink ⇒ ScalaSink}
 import com.microsoft.azure.iot.iothubreact.checkpointing.CheckpointService.UpdateOffset
-import com.microsoft.azure.iot.iothubreact.checkpointing.{CheckpointActorSystem, IOffsetLoader, OffsetLoader}
-import com.microsoft.azure.iot.iothubreact.checkpointing.backends.CheckpointBackend
-import com.microsoft.azure.iot.iothubreact.config.{Configuration, IConfiguration}
+import com.microsoft.azure.iot.iothubreact.checkpointing.{CheckpointActorSystem, IOffsetLoader}
+import com.microsoft.azure.iot.iothubreact.config.IConfiguration
 import com.microsoft.azure.iot.iothubreact.{Logger, MessageFromDevice}
 
 import scala.collection.concurrent.TrieMap
@@ -26,20 +23,9 @@ final case class OffsetSaveSink(parallelism: Int, config: IConfiguration, offset
   }(collection.breakOut): Map[Int, ActorRef]
 
   val current: TrieMap[Int, Long] = TrieMap()
-  offsetLoader.GetSavedOffsets.foreach{ case (a, c) ⇒
-    current += a → c.toLong
-  }
-
-  private[this] object JavaSinkProcedure extends Procedure[MessageFromDevice] {
-    @scala.throws[Exception](classOf[Exception])
-    override def apply(m: MessageFromDevice): Unit = {
-      doWrite(m)
-    }
-  }
-
-  def scalaSink(): ScalaSink[MessageFromDevice, scala.concurrent.Future[Done]] = {
-    import scala.concurrent.ExecutionContext.Implicits.global
-    ScalaSink.foreachParallel[MessageFromDevice](parallelism) { doWrite }
+  offsetLoader.GetSavedOffsets.foreach {
+    case (a, c) ⇒
+      current += a → c.toLong
   }
 
   private def doWrite(m: MessageFromDevice) = {
@@ -59,9 +45,14 @@ final case class OffsetSaveSink(parallelism: Int, config: IConfiguration, offset
     }
   }
 
+  def scalaSink(): ScalaSink[MessageFromDevice, scala.concurrent.Future[Done]] = {
+    import scala.concurrent.ExecutionContext.Implicits.global
+    ScalaSink.foreachParallel[MessageFromDevice](parallelism) { doWrite }
+  }
+
   def javaSink(): JavaSink[MessageFromDevice, CompletionStage[Done]] = {
     JavaSink.foreach[MessageFromDevice] {
-      JavaSinkProcedure
+      doWrite
     }
   }
 }
